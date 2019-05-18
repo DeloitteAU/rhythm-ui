7af7c17169ccdff5793bb20a3e1df37608392919
Jenna Salau <jsalau@deloitte.com.au>
2019-05-18 13:37:46 +1000

    Configured gatsby

---
+++ www/gatsby-config.js
@@ -0,0 +1,53 @@
+module.exports = {
+	siteMetadata: {
+		title: 'Gatsby Default Starter',
+		description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+		author: '@gatsbyjs',
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: `${__dirname}/src/images`,
+			},
+		},
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'components',
+				path: `${__dirname}/../packages`,
+			},
+		},
+
+		'gatsby-mdx',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: 'gatsby-starter-default',
+				short_name: 'starter',
+				start_url: '/',
+				background_color: '#663399',
+				theme_color: '#663399',
+				display: 'minimal-ui',
+				icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+			},
+		},
+		// {
+		// 	resolve: `gatsby-plugin-typescript`,
+		// 	options: {
+		// 	  isTSX: true, // defaults to false
+		// 	  jsxPragma: `jsx`, // defaults to "React"
+		// 	  allExtensions: true, // defaults to false
+		// 	},
+		//   },
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// 'gatsby-plugin-offline',
+
+
+	],
+};
