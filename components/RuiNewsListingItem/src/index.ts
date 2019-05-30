4ca3ca05ea4be97482e0fc6673b30255bc0dc08f
jnelssonsmith <j.nelssonsmith@gmail.com>
2019-05-31 09:57:26 +1000

    Revert "WIP rui news item"
    This reverts commit e5c598a0801e9c5dfe898408787bb26a364285c9.

--- components/RuiNewsListingItem/src/index.ts
+++
@@ -1,6 +0,0 @@
-import {RuiNewsListingItem} from './RuiNewsListingItem';
-
-// Register the new element with the browser.
-customElements.define(RuiNewsListingItem.tag, RuiNewsListingItem);
-
-export default RuiNewsListingItem;
