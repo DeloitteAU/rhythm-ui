f8c512f108b4cd89d174d20c23837d9489e4105a
Jenna Salau <jsalau@deloitte.com.au>
2019-06-14 09:33:07 +1000

    Disabled any rule

--- www/src/templates/Default/Default.tsx
+++ www/src/templates/Default/Default.tsx
@@ -1,39 +1,39 @@
-import React from "react"
+import React from 'react';
 import RuiLayout from '@rhythm-ui/layout-react';
-import { StaticQuery, graphql, Link } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import {Navigation} from '../../components/Navigation';
 import {Header} from '../../components/Header';
 import {Footer} from '../../components/Footer';
 
-export const Default = ({ children }) => (
+export const Default = ({children}) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-        allMdx {
-          nodes {
-            id
-            fields {
-              nodeTitle
-              relativeUrlPath
-            }
-          } 
-        }
-      }
-    `}
-    render={data => (
-      <RuiLayout type="picasso">
-        <Header />
-        <main>
-          {children}
-        </main>
-        <Navigation />
-        <Footer />
-      </RuiLayout>
-    )}
+	query={graphql`
+	  query SiteTitleQuery {
+		site {
+		  siteMetadata {
+			title
+		  }
+		}
+		allMdx {
+		  nodes {
+			id
+			fields {
+			  nodeTitle
+			  relativeUrlPath
+			}
+		  }
+		}
+	  }
+	`}
+	render={data => (
+	  <RuiLayout type="picasso">
+		<Header />
+		<main>
+		  {children}
+		</main>
+		<Navigation />
+		<Footer />
+	  </RuiLayout>
+	)}
   />
-)
+);
