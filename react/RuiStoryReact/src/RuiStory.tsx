6f94feba0a3f022c7eb0265ff93a6be595ca1bf5
jnelssonsmith <j.nelssonsmith@gmail.com>
2019-06-14 12:34:26 +1000

    Remove redundant import

--- react/RuiStoryReact/src/RuiStory.tsx
+++ react/RuiStoryReact/src/RuiStory.tsx
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { ReactNode } from 'react';
+import React from 'react';
 import '@rhythm-ui/story';
 
 import {IRuiStoryProps} from './IRuiStory';
