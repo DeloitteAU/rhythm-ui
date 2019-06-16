de6a3bb2e5b198bc94fcbd974766583b17b09e3f
Jenna Salau <jsalau@deloitte.com.au>
2019-06-16 23:56:08 +1000

    Single run test

--- karma.conf.js
+++ karma.conf.js
@@ -109,7 +109,7 @@ module.exports = function(config) {
 		logLevel: config.LOG_DEBUG,// config.LOG_INFO,
 		autoWatch: true,
 		browsers: ['Chrome' /*, 'Firefox', 'Edge'*/],
-		singleRun: false,
+		singleRun: true,
 		concurrency: Infinity
 	});
 };
