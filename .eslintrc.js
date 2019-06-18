134a818fc729b8cc00a4cf76bfb0b69aaa6f08a1
Jenna Salau <jsalau@deloitte.com.au>
2019-06-18 22:32:24 +1000

    Updated build command

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
-  plugins: ['@typescript-eslint'],
-  env: {
-    browser: true,
-    node: true,
-    jest: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    '@typescript-eslint/interface-name-prefix': [2, 'always'],
-	'@typescript-eslint/no-namespace': [0, {}],
-  }
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'prettier/@typescript-eslint',
+	],
+	plugins: ['@typescript-eslint'],
+	env: {
+		browser: true,
+		node: true,
+		jest: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		'@typescript-eslint/interface-name-prefix': [2, 'always'],
+		'@typescript-eslint/no-namespace': [0, {}],
+	}
 };
