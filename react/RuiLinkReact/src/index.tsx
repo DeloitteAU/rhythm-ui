6be0d98ac1d37722a13d90c12d41240d462c1ce7
Georgia Mills <gmills@deloitte.com.au>
2019-06-24 16:26:21 +1000

    removed default

--- react/RuiLinkReact/src/index.tsx
+++ react/RuiLinkReact/src/index.tsx
@@ -4,7 +4,7 @@
 * This source code is licensed under the BSD-3-Clause license found in the
 * LICENSE file in the root directory of this source tree.
 */
-import { RuiLink } from './RuiLink';
+import RuiLink from './RuiLink';
 
 export * from './RuiLink';
 export default RuiLink;
