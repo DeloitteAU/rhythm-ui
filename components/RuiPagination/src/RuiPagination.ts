06b5124226c90d9954f6b1a00af01f73f4c6b311
jnelssonsmith <j.nelssonsmith@gmail.com>
2019-07-05 11:21:48 +1000

    A less buggy pagination item calculation method

--- components/RuiPagination/src/RuiPagination.ts
+++ components/RuiPagination/src/RuiPagination.ts
@@ -38,6 +38,57 @@ export class RuiPagination extends LitElement {
 		return [variables, layout];
 	}
 
+
+	private calculatePageStartEnd() {
+		let pageStart = 1;
+		let pageEnd = this.numberOfPages;
+
+		if (this.pagesToShow >= this.numberOfPages) {
+			return [pageStart, pageEnd];
+		}
+
+		// the number of items that should show to the left of the current page
+		const leftShown = Math.floor(this.pagesToShow / 2);
+		
+		// the render logic is slightly different when we are in the 
+		// part of the app where ellipses are not shown
+		const leftSideThreshold = leftShown + 2;
+		
+		if (this.currentPage <= leftSideThreshold) {
+			// the number of pagination items that should show is 
+			// defined by pages to show + 4 where 
+			// the 4 comes from the start and end item which are always present,
+			// and the two ellipses slots which may or may not be actual ellipses
+			// e.g 1 | ... | pages-to-show items | ... | n
+			const MAX_ITEMS = Math.min(this.pagesToShow + 4, this.numberOfPages);
+			pageEnd = this.currentPage + (MAX_ITEMS - 2 - this.currentPage);
+		} else {
+			const rightShown = this.pagesToShow - leftShown - 1;
+			const rightSideThreshold = this.numberOfPages - rightShown - 1;
+
+			let rightTruncate = 0;
+			if (this.currentPage >= rightSideThreshold) {
+				rightTruncate = rightShown - (this.numberOfPages - this.currentPage) + 2
+			}
+			
+			pageStart = Math.max(this.currentPage - leftShown - rightTruncate, 1);
+			pageEnd =  Math.min(this.currentPage + rightShown, this.numberOfPages);
+
+			if (pageStart === 3) {
+				pageStart = 2;
+			}
+
+			if (pageEnd === this.numberOfPages - 2) {
+				pageEnd = this.numberOfPages - 1;
+			}
+		}
+
+		return [
+			pageStart,
+			pageEnd
+		];
+	}
+
 	/* #endregion */
 
 	/* #region Methods */
@@ -47,38 +98,9 @@ export class RuiPagination extends LitElement {
 		* @slot This is a slot test
 	*/
 	public render(): TemplateResult {
-		const leftShown = Math.floor(this.pagesToShow / 2);
-		const rightShown = this.pagesToShow - leftShown - 1;
-		let pageStart = Math.max(this.currentPage - leftShown, 1);
-		let pageEnd =  Math.min(this.currentPage + rightShown, this.numberOfPages);
-
-
-		const leftTruncate = leftShown - (pageStart - 1);
-		if (leftTruncate > 0) {
-			pageEnd = Math.min(pageEnd + leftTruncate, this.numberOfPages);
-		}
-
-		const rightTruncate = rightShown - (this.numberOfPages - pageEnd);
-		if (rightTruncate > 0) {
-			pageStart = Math.max(pageStart - rightTruncate, 1);
-		}
-
-		if (this.currentPage <= leftShown) {
-			pageEnd += 1 + leftShown - this.currentPage
-		}
-
-		if (this.currentPage >= this.numberOfPages - rightShown) {
-			pageStart -= rightShown - (this.numberOfPages - this.currentPage)
-		}
-
-		if (pageStart === 3) {
-			pageStart = 2;
-		}
-
-		if (pageEnd === this.numberOfPages - 2) {
-			pageEnd = this.numberOfPages - 1;
-		}
-
+		
+		const [pageStart, pageEnd] = this.calculatePageStartEnd();
+	
 		let renderLeftEllipses = false;
 		let renderRightEllipses = false;
 
