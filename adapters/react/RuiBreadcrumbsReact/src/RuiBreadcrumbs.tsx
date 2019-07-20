446630478a933aafda1219699ee9fa08354e7633
jnelssonsmith <j.nelssonsmith@gmail.com>
2019-07-20 12:08:26 +1000

    Adds more documentation to react adaptor

--- adapters/react/RuiBreadcrumbsReact/src/RuiBreadcrumbs.tsx
+++ adapters/react/RuiBreadcrumbsReact/src/RuiBreadcrumbs.tsx
@@ -15,8 +15,7 @@ import {
 } from './IRuiBreadcrumbs';
 
 /**
- * Breadcrumb Crumb wrapps the rui-link in an anchor element by default which is passed into the slot "crumb".
- * It is then exposed as Crumb. to be used as Breadcrumbs.Crumb
+ * The Crumb subcomponent is used when the user wants to pass in custom breadcrumb elements
  */
 export const Crumb = (props: IRuiBreadcrumbsCrumbProps): React.ReactNode => {
 	const {as, truncatedLabel, ...otherProps} = props;
@@ -40,6 +39,9 @@ export const Crumb = (props: IRuiBreadcrumbsCrumbProps): React.ReactNode => {
 	return crumbEl;
 };
 
+/**
+ * The Seperator subcomponent is used if the user wants to pass in a custom seperator element
+ */
 export const Seperator = (props: IRuiBreadcrumbsSeperatorProps): React.ReactNode => {
 	const {as, ...otherProps} = props;
 
@@ -56,30 +58,74 @@ export const Seperator = (props: IRuiBreadcrumbsSeperatorProps): React.ReactNode
 	return crumbEl;
 };
 
+/**
+ * React adaptor for <rui-breadcrumbs /> component
+ */
 export class RuiBreadcrumbs extends React.Component<IRuiBreadcrumbsProps> {
 	// used to add event listeners to element
 	private ruiBreadcrumbsEl = React.createRef<HTMLElement>();
 
+	// Crumb subcomponent accessed via RuiBreadcrumbs.Crumb
 	public static Crumb = Crumb;
+
+	// Seperator subcomponent accessed via RuiBreadcrumbs.Seperator
 	public static Seperator = Seperator;
 
+
+	/**
+	 * Click handler for when the crumb item click event is fired
+	 */
+	private _handleCrumbClick = (e: CustomEvent): void => {
+		this.props.onCrumbClick(e.detail.crumbIndex);
+	}
+
+	/**
+	 * Click handler for when the crumb item selected event is fired
+	 */
+	private _handleCrumbSelect = (e: CustomEvent): void => {
+		this.props.onCrumbSelect(e.detail.crumbIndex);
+	}
+
+	/**
+	 * If the user has specified a custom click or select handler then
+	 * we attach event listeners and fire coresponding handler functions
+	 */
 	public componentDidMount(): void {
 		const {onCrumbSelect, onCrumbClick} = this.props;
-		if (onCrumbSelect || onCrumbClick) {
-			const el: HTMLElement | null = this.ruiBreadcrumbsEl.current;
-			if (el) {
-				if (onCrumbClick) {
-					el.addEventListener('rui-breadcrumbs-item-click', (e: CustomEvent): void => { onCrumbClick(e.detail.crumbIndex); });
-				}
+		const el: HTMLElement | null = this.ruiBreadcrumbsEl.current;
+		if ((onCrumbSelect || onCrumbClick) && el) {
+			if (onCrumbClick) {
+				el.addEventListener('rui-breadcrumbs-item-click', this._handleCrumbClick);
+			}
 
-				if (onCrumbSelect) {
-					el.addEventListener('rui-breadcrumbs-item-select', (e: CustomEvent): void => { onCrumbSelect(e.detail.crumbIndex); });
-				}
+			if (onCrumbSelect) {
+				el.addEventListener('rui-breadcrumbs-item-select', this._handleCrumbSelect);
 			}
 		}
 	}
 
-	public render() {
+	/**
+	 * Clean up any event listeners if any are set
+	 */
+	public componentWillUnmount(): void {
+		const {onCrumbSelect, onCrumbClick} = this.props;
+		const el: HTMLElement | null = this.ruiBreadcrumbsEl.current;
+
+		if ((onCrumbSelect || onCrumbClick) && el) {
+			if (onCrumbClick) {
+				el.removeEventListener('rui-breadcrumbs-item-click', this._handleCrumbClick);
+			}
+
+			if (onCrumbSelect) {
+				el.removeEventListener('rui-breadcrumbs-item-select', this._handleCrumbSelect);
+			}
+		}
+	}
+
+	/**
+	 * Render rui-breadcrumbs component, with any attribute name conversions applied
+	 */
+	public render(): React.ReactNode {
 		const {maxCrumbs, onCrumbSelect, onCrumbClick, ...otherProps} = this.props;
 
 		const props = otherProps;
