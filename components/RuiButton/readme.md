0d2fd120a7dad7c615904c7076b77c2f1c0fdcc2
Rico Minten <ricominten@gmail.com>
2019-08-19 21:32:34 +1000

    Add icon to button component Add custom CSS for button icon Re-organise button prop order Fix icons

--- components/RuiButton/readme.md
+++ components/RuiButton/readme.md
@@ -3,52 +3,78 @@ package: "@rhythm-ui"
 title: "rui-button"
 ---
 
-# button
+# Button
 
 Buttons provide a clickable element, which can be used in forms, or anywhere that needs simple, standard button functionality. They may display text, icons, or both. Buttons can be styled with several attributes to look a specific way.
 
 ## Types
+Type options are: **primary|secondary|tertiary** (if nothing is specified the default will be set)
 
 ```html preview
-<rui-button>Default</rui-button>
-<rui-button type="primary">Primary</rui-button>
+<rui-button type="primary">Primary (default)</rui-button>
 <rui-button type="secondary">Secondary</rui-button>
 <rui-button type="tertiary">Tertiary</rui-button>
 ```
 
 ## Variants
-
+Variant options are: **fill|outline|tertiary** (if nothing is specified the default will be set)
 ```html preview
 <rui-button variant="fill">Fill (default)</rui-button>
 <rui-button variant="outline">Outline</rui-button>
 <rui-button variant="ghost">Ghost</rui-button>
 ```
 
-## Behaviour
-
-```html preview
-<rui-button behaviour="button">Button (default)</rui-button>
-<rui-button behaviour="submit">Form Submit</rui-button>
-<rui-button behaviour="reset">Form Reset</rui-button>
-<rui-button behaviour="anchor">Anchor</rui-button>
-<rui-button href="#">Href</rui-button>
-```
-
-## Disabled
-
-```html preview
-<rui-button disabled>Disabled</rui-button>
-<rui-button variant="outline" disabled>Outline</rui-button>
-<rui-button variant="ghost" disabled>Ghost</rui-button>
-```
-
 ## Sizes
+Size options are: **small|normal|large** (if nothing is specified the default will be set)
 
 ```html preview
 <rui-button size="small">Small</rui-button>
 <rui-button size="normal">Normal (default)</rui-button>
 <rui-button size="large">Large</rui-button>
 ```
+
+## Disabled
+Button can be disabled by using the disabled prop
+```html preview
+<rui-button disabled>Disabled</rui-button>
+<rui-button variant="outline" disabled>Outline</rui-button>
+<rui-button variant="ghost" disabled>Ghost</rui-button>
+```
+> (Disabled is not supported with the anchor behaviour due to a tags not supporting disabled properties)
+
+
+## Behaviour
+Behaviour options are: **button|submit|reset|anchor** (if nothing is specified the default will be set unless an href has been specified)
+```html preview
+<rui-button behaviour="button">Button (default)</rui-button>
+<rui-button behaviour="submit">Form Submit</rui-button>
+<rui-button behaviour="reset">Form Reset</rui-button>
+<rui-button behaviour="anchor">Anchor</rui-button>
+<rui-button href="#">Href</rui-button>
+<rui-button onClick="() => {window.alert('button clicked')}"}">onClick</rui-button>
+
+```
+
+## With icons
+Button can accept iconography by adding the icon in the desired location relative to the text, styling the icon is like styling any regular nested html element.
+```html preview
+<rui-button>Proceed <rui-icon src="http://pluspng.com/img-png/right-arrow-png-right-arrow-512.png"></rui-icon></rui-button>
+<rui-button class="rui-btn-icon-left" variant="outline"><rui-icon src="https://image.flaticon.com/icons/svg/54/54321.svg"></rui-icon>Back</rui-icon></rui-button>
+```
+```css
+rui-button rui-icon {
+	margin-left: 15px;
+	--rui-icon__display: inline-block;
+	--rui-icon__width: 12px;
+	--rui-icon__height: 12px;
+	--rui-icon__color: #ffffff;
+}
+rui-button:hover rui-icon {
+	--rui-icon__color: #000000;
+}
+
+```
+
 
 ## Extending
 
--- components/RuiButton/src/RuiButton.ts
+++ components/RuiButton/src/RuiButton.ts
@@ -14,94 +14,91 @@ import {variables, layout} from './RuiButton.css'
  */
 export class RuiButton extends LitElement {
 
-  /* #region Properties */
+	/* #region Properties */
 
-  /**
-   * The underlying type of the button.
-   */
-  @property({type : String})
-  public behaviour: 'submit' | 'reset' | 'button' | 'anchor' = 'button';
+	/**
+	 * The theme type of the button
+	 */
+	@property({type: String})
+	public type: 'default' | 'primary' | 'secondary' | 'tertiary' = 'primary';
 
-  /**
-   * The theme type of the button
-   */
-  @property({type : String})
-  public type?: 'default' | 'primary' | 'secondary' | 'tertiary' = 'primary';
+	/**
+	 * The variant style of the button
+	 */
+	@property({type: String})
+	public variant: 'outline' | 'ghost' | 'fill' = 'fill';
 
-    /**
-   * The variant style of the button
-   */
-  @property({type : String})
-  public variant?: 'outline' | 'ghost' | 'fill' = 'fill';
+	/**
+	 * The size of the button
+	 */
+	@property({type: String})
+	public size: 'normal' | 'small' | 'large' = 'normal';
 
-  /**
-   * Set to true if the button should be disabled
-   */
-  @property({type : Boolean})
-  public disabled: boolean = false;
+	/**
+	 * Set to true if the button should be disabled
+	 */
+	@property({type: Boolean})
+	public disabled: boolean = false;
 
-  /**
-   * Href location for the button
-   * @remarks
-   * This forces the element to render using an anchor tag in the shadow dom
-   */
-  @property({type : String})
-  public href? = undefined;
+	/**
+	 * The underlying type of the button.
+	 */
+	@property({type: String})
+	public behaviour: 'submit' | 'reset' | 'button' | 'anchor' = 'button';
 
-  /**
-   * The size of the button
-   */
-  @property({type : String})
-  public size?: 'normal' | 'small' | 'large';
+	/**
+	 * Href location for the button
+	 * @remarks
+	 * This forces the element to render using an anchor tag in the shadow dom
+	 */
+	@property({type: String})
+	public href? = undefined;
 
-  /**
-   *
-   * The styles for button
-   * @remarks
-   * If you are extending this class you can extend the base styles with super. Eg `return [super(), myCustomStyles]`
-   */
-  public static get styles(): CSSResultArray {
-    return [variables, layout];
-  }
+	/**
+	 *
+	 * The styles for button
+	 * @remarks
+	 * If you are extending this class you can extend the base styles with super. Eg `return [super(), myCustomStyles]`
+	 */
+	public static get styles(): CSSResultArray {
+		return [variables, layout];
+	}
 
-  /* #endregion */
+	/* #endregion */
 
 
-  /* #region Methods */
+	/* #region Methods */
 
-  /**
-   * Render method
-   * @slot This is a slot test
-   */
-  public render(): TemplateResult {
+	/**
+	 * Render method
+	 * @slot This is a slot test
+	 */
+	public render(): TemplateResult {
 
-    // If href has not been defined use <button>
-    if (!this.href && this.behaviour !== 'anchor') {
-      return html`
-        <button
-          class="btn"
-          type=${this.behaviour}
-          part="btn"
-          ?disabled=${this.disabled}
-        >
-          <slot></slot>
-        </button>
-    `;
-    }
+// If href has not been defined use <button>
+		if (!this.href && this.behaviour !== 'anchor') {
+			return html`
+				<button
+					class="btn"
+					?disabled=${this.disabled}
+				>
+					<slot></slot>
+				</button>
+			`;
+		}
 
-    // Use <a>
-    return html`
-      <a
-        class="btn"
-        part="btn"
-        href=${ifDefined(this.href)}
-      >
-        <slot></slot>
-      </a>
-    `;
-  }
+// Use <a>
+		return html`
+			<a
+				class="btn"
+				href=${ifDefined(this.href)}
+			>
+				<slot></slot>
+			</a>
+		`;
+	}
 
-  /* #endregion */
+	/* #endregion */
 
 }
 
