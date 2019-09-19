9267bf1d300a1bf234a3d8ffbc398e1820c385ea
Jenna Salau <jsalau@deloitte.com.au>
2019-09-19 23:28:05 +1000

    Update publish to staging

--- adapters/vue/.storybook/webpack.config.js
+++ adapters/vue/.storybook/webpack.config.js
@@ -2,6 +2,7 @@
 const updateWebpackConfig = require('storybook-readme/vue/updateWebpackConfig');
 
 module.exports = ({config}) => {
+  config.output.publicPath = process.STORYBOOK_PATH_PREFIX || '/';
   config.resolve.extensions.push('.ts', '.vue', 'tsx', '.css', '.scss', '.sass', '.html')
   config.module.rules.push({
     test: /\.ts$/,
