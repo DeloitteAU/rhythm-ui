192b1ab21747d3623a4963922e5849e7627594cb
Rico Minten <ricominten@gmail.com>
2019-11-25 09:41:10 +1100

    Update tests

--- components/RuiAccordion/tests/RuiAccordion.test.ts
+++ components/RuiAccordion/tests/RuiAccordion.test.ts
@@ -9,6 +9,21 @@
 import '../src';
 import TestUtils from '../../../TestUtils';
 
+const title = '<h2 slot="heading">accordion</h2>';
+const button = '<button slot="button-toggle">button</button>';
+const el1 = `<rui-accordion-item open id="el1">
+					<span slot="summary-content">title1</span>
+					<p id="content1" slot="details-content">content1</p>
+				</rui-accordion-item>`;
+const el2 = `<rui-accordion-item id="el2">
+					<span slot="summary-content">title2</span>
+					<p id="content2"  slot="details-content">content2</p>
+				</rui-accordion-item>`;
+const el3 = `<rui-accordion-item id="el3">
+					<span slot="summary-content">title3</span>
+					<p id="content3"  slot="details-content">content3</p>
+				</rui-accordion-item>`;
+
 describe('RuiAccordion', () => {
 
 	it('Renders', async () => {
@@ -17,17 +32,29 @@ describe('RuiAccordion', () => {
 		expect(ele).toBeDefined();
 		expect(ele.shadowRoot).toBeDefined();
 	})
-	it('Renders all slot elements', async () => {
-		const ele = await TestUtils.render('rui-accordion', {}, '');
+	it('Renders all slot elements correctly', async () => {
+		const ele = await TestUtils.render('rui-accordion', 	{}, `${title}${button}${el1}${el2}`);
 
 		expect(ele).toBeDefined();
 		expect(ele.shadowRoot).toBeDefined();
+
+		expect(ele.innerHTML.includes('accordion')).toBeTruthy();
+		expect(ele.innerHTML.includes('button')).toBeTruthy();
+		expect(ele.innerHTML.includes('title1')).toBeTruthy();
+		expect(ele.innerHTML.includes('content1')).toBeTruthy();
+		expect(document.getElementById( 'content1' ).offsetParent).not.toEqual(null); // Using offsetParent to evaluate visibility of element
+		expect(ele.innerHTML.includes('title2')).toBeTruthy();
+		expect(ele.innerHTML.includes('content2')).toBeTruthy();
+		expect(document.getElementById( 'content2' ).offsetParent).toEqual(null);
 	})
 	it('Works with single behaviour', async () => {
-		const ele = await TestUtils.render('rui-accordion', {}, '');
+		const ele = await TestUtils.render('rui-accordion', 	{}, `${el1}${el2}`);
 
 		expect(ele).toBeDefined();
 		expect(ele.shadowRoot).toBeDefined();
+
+		expect(document.getElementById( 'content1' ).offsetParent).not.toEqual(null); // Using offsetParent to evaluate visibility of element
+		expect(document.getElementById( 'content2' ).offsetParent).toEqual(null);
 	})
 	it('Works with multiple behaviour', async () => {
 		const ele = await TestUtils.render('rui-accordion', {}, '');
